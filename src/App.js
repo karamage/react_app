ca731d8304438ea912b5fa6cb97a91b45d253d9a
karamage <karamage.umauma@gmail.com>
2020-11-28 15:51:00 +0900

    devide component.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Rect from './Rect';
 import './App.css';
 
 class App extends Component {
@@ -9,36 +10,6 @@ class App extends Component {
       <Rect x="150" y="100" w="150" h="150" c="magenta" />
       <Rect x="100" y="150" w="150" h="150" c="black" />
     </div>;
-  }
-}
-
-class Rect extends Component {
-  x = 0;
-  y = 0;
-  width = 0;
-  height = 0;
-  color = "white";
-  style = {};
-
-  constructor(props) {
-    super(props);
-    this.x = props.x;
-    this.y = props.y;
-    this.width = props.w;
-    this.height = props.h;
-    this.color = props.c;
-    this.style = {
-      backgroundColor: this.color,
-      position: "absolute",
-      left: this.x + "px",
-      top: this.y + "px",
-      width: this.width + "px",
-      height: this.height + "px"
-    }
-  }
-
-  render() {
-    return <div style={this.style}></div>;
   }
 }
 
