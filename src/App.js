346eaa354798eac677ff7c37c06c88321dfea463
karamage <karamage.umauma@gmail.com>
2020-11-28 17:10:06 +0900

    add switch style

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  msgStyle = {
+  msgStyle1 = {
     fontSize: "24px",
     color: "#900",
+    margin: "20px 0px",
+    padding: "5px",
+    borderBottom: "2px solid #900"
+  }
+  msgStyle2 = {
+    fontSize: "20px",
+    color: "white",
+    backgroundColor: "#900",
     margin: "20px 0px",
     padding: "5px",
     borderBottom: "2px solid #900"
@@ -19,6 +27,7 @@ class App extends Component {
     this.state = {
       counter: 0,
       msg: 'count start',
+      flg: true,
     };
     this.doAction = this.doAction.bind(this);
   }
@@ -26,14 +35,19 @@ class App extends Component {
   doAction(e) {
     this.setState((state) => ({
       counter: state.counter + 1,
-      msg: 'count: ' + state.counter
+      msg: 'count: ' + state.counter,
+      flg: !state.flg
     }));
   }
 
   render() {
     return <div>
       <h1>React</h1>
-      <p style={this.msgStyle}>{this.state.msg}</p>
+      {this.state.flg ?
+        <p style={this.msgStyle1}>{this.state.msg}</p>
+        :
+        <p style={this.msgStyle2}>{this.state.msg}です。</p>
+      }
       <button style={this.btnStyle} onClick={this.doAction}>Click</button>
     </div>;
   }
