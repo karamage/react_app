bf9f94de3e3b71f21bb4be639f7c633be7f40b75
karamage <karamage.umauma@gmail.com>
2020-11-28 16:09:46 +0900

    add radius

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ class App extends Component {
   render() {
     return <div>
       <h1>React</h1>
-      <Rect x="50" y="50" w="150" h="150" c="cyan" />
-      <Rect x="150" y="100" w="150" h="150" c="magenta" />
-      <Rect x="100" y="150" w="150" h="150" c="black" />
+      <Rect x="50" y="50" w="150" h="150" c="cyan" r="50" />
+      <Rect x="150" y="100" w="150" h="150" c="magenta" r="75"/>
+      <Rect x="100" y="150" w="150" h="150" c="black" r="25"/>
     </div>;
   }
 }
