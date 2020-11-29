f588b04750bd1c99493ad393849593ceecc9f8d7
karamage <karamage.umauma@gmail.com>
2020-11-29 17:40:18 +0900

    improve redux.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,83 @@
 import React, { Component } from 'react';
-import Rect from './Rect'
+// import Rect from './Rect'
+import { connect } from 'react-redux';
 import './App.css';
 
+// ステートのマッピング
+function mappingState(state) {
+  return state;
+}
+
+// Appコンポーネント
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Redux</h1>
+        <Message />
+        <Button />
+      </div>
+    );
+  }
+}
+
+// ストアのコネクト
+App = connect()(App);
+
+// メッセージ表示のコンポーネント
+class Message extends Component {
+  style = {
+    fontSize: "20px",
+    padding: "20px 5px"
+  }
+
+  render() {
+    return (
+      <p style={this.style}>
+        {this.props.message}: {this.props.counter}
+      </p>
+    );
+  }
+}
+// ストアのコネクト
+Message = connect(mappingState)(Message);
+
+// ボタン表示のコンポーネント
+class Button extends Component {
+  style = {
+    fontSize: "16px",
+    padding: "5px 10px"
+  }
+
+  constructor(props) {
+    super(props);
+    this.doAction = this.doAction.bind(this);
+  }
+
+  // ボタンクリックでディスパッチを実行
+  doAction(e) {
+    if (e.shiftKey) {
+      this.props.dispatch({ type: 'DECREMENT' });
+    } else {
+      this.props.dispatch({ type: 'INCREMENT' });
+    }
+  }
+
+  render() {
+    return (
+      <button style={this.style} onClick={this.doAction}>click</button>
+    );
+  }
+}
+
+// ストアのコネクト
+Button = connect()(Button);
+
+/*
 let data = {title: 'Title', message: 'this is sample message.'};
 
 const SampleContext = React.createContext(data);
@@ -49,5 +125,6 @@ class Message extends Component {
     );
   }
 }
+*/
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
+
+// ステートの値
+let state_value = {
+  counter: 0,
+  message: "COUNTER"
+}
+
+// レデューサー
+function counter(state = state_value, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        counter: state.counter + 1,
+        message: "INCREMENT"
+      };
+    case 'DECREMENT':
+      return {
+        counter: state.counter - 1,
+        message: "DECREMENT"
+      };
+    default:
+      return state;
+  }
+}
+
+// ストアを作成
+let store = createStore(counter);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App msg="Hello App." />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
